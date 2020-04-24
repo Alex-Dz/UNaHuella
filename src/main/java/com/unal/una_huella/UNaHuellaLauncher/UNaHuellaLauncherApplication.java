b7ece23d30ce7ff6aeba00984314cc430a59c7ad
alejandrasuperlano <rsuperlano@unal.edu.co>
2020-04-23 19:42:52 -0500

    prueba en Testing branch

--- src/main/java/com/unal/una_huella/UNaHuellaLauncher/UNaHuellaLauncherApplication.java
+++ src/main/java/com/unal/una_huella/UNaHuellaLauncher/UNaHuellaLauncherApplication.java
@@ -2,6 +2,7 @@ package com.unal.una_huella.UNaHuellaLauncher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//prueba testing branch
 
 @SpringBootApplication
 public class UNaHuellaLauncherApplication {
