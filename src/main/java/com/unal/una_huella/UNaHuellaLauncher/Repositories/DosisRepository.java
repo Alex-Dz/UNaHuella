13c484919a1970414be814a40763f8e6c2ceb092
Alex <dicardenasd@unal.edu.co>
2020-04-24 16:30:48 -0500

    Agregados Repository de entidades -CitaRepository -DosisRepository -LugarRepository -MedicamentoRepository

---
+++ src/main/java/com/unal/una_huella/UNaHuellaLauncher/Repositories/DosisRepository.java
@@ -0,0 +1,8 @@
+package com.unal.una_huella.UNaHuellaLauncher.Repositories;
+
+import com.unal.una_huella.UNaHuellaLauncher.Entities.Dosis;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DosisRepository extends CrudRepository<Dosis, Integer> {
+
+}
---
+++ src/main/java/com/unal/una_huella/UNaHuellaLauncher/Repositories/MedicamentoRepository.java
@@ -0,0 +1,8 @@
+package com.unal.una_huella.UNaHuellaLauncher.Repositories;
+
+import com.unal.una_huella.UNaHuellaLauncher.Entities.Medicamento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MedicamentoRepository extends CrudRepository<Medicamento, Integer> {
+
+}
