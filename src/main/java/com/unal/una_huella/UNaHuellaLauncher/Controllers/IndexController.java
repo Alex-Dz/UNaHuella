f0757e65982090bce86bb953a4179c1e2d578ac2
Alejandra Superlano <rsuperlano@unal.edu.co>
2020-05-31 03:58:45 -0500

    - clases btn-flat agregadas a stylesheet.css - pagina misMascotas de particular adelantada - indexcontroller modificado para visualizar misMascotas

--- src/main/java/com/unal/una_huella/UNaHuellaLauncher/Controllers/IndexController.java
+++ src/main/java/com/unal/una_huella/UNaHuellaLauncher/Controllers/IndexController.java
@@ -30,4 +30,9 @@ public class IndexController {
     String asignarCita() {
         return "asignarCita";
     }
+
+    @RequestMapping("/misMascotas")
+    String misMascotas() {
+        return "misMascotas";
+    }
 }
